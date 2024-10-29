01a93339659e51fc7a9cb6f8f6b107422d776736
tatata812 <75353660+tatata812@users.noreply.github.com>
2024-10-29 09:44:31 +0900

    jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,24 @@ $(function () {
   });
 
 
+// ヘッダーロゴの動き
+  $('.header__logo-wrap').hide();
+  // スクロールイベントの設定
+  $(window).scroll(function() {
+      // .contents-sectionの位置を取得
+      var infoSectionTop = $('.contents-section').offset().top;
+      // 現在のスクロール位置を取得
+      var scrollTop = $(window).scrollTop();
+
+      // スクロール位置が.contents-sectionに達したら
+      if (scrollTop >= infoSectionTop) {
+          // .header__logo-wrapをふわっと表示
+          $('.header__logo-wrap').fadeIn(500);
+      }
+  });
+  
+
+
   $(".top-to-js").click(function () {
     $("body,html").animate({
         scrollTop: 0 //ページトップまでスクロール
@@ -35,25 +53,6 @@ $(function () {
     return false; //親要素へのイベント伝播を止める
   });
 
-  $('.top-style-js').slick({
-    autoplay: true, // 自動でスクロール
-    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
-    speed: 5000, // スライドが流れる速度を設定
-    cssEase: "linear", // スライドの流れ方を等速に設定
-    slidesToShow: 4, // 表示するスライドの数
-    swipe: false, // 操作による切り替えはさせない
-    arrows: false, // 矢印非表示
-    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
-    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
-    responsive: [
-      {
-        breakpoint: 750,
-        settings: {
-          slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
-        }
-      }
-    ]
-  });
 
   // ページ内リンクヘッダーの高さ
   var headerHeight = 60; // ヘッダーの高さ
